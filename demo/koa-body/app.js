b731431a66bd4e7b516ebe64d6920369e0e03839
samcandy <samcandy841204@gmail.com>
2017-07-25 20:29:34 +0800

    2017-7-24-demo

--- demo/koa-body/app.js
+++ demo/koa-body/app.js
@@ -1,21 +1,25 @@
+//使用 koa framwork
 var koa = require('koa');
+//
 var Router=require('koa-router');
-
+//bodyparser使用在POST
 var bodyparser = require('koa-bodyparser');
 
 var app = koa();
 var router = new Router();
 
 app.use(bodyparser());
-//app.keys = ['ddddddddddaaaaaaaa'];
-//app.use(session())
+/*
+app.keys = ['ddddddddddaaaaaaaa'];
+app.use(session())
 
-//app.use(function * (){
-//    var count = this.session.count || 0;
-//    count ++;
-//    this.session.count = count;
-//    this.body = this.session.count;
-//});
+app.use(function * (){
+    var count = this.session.count || 0;
+    count ++;
+    this.session.count = count;
+    this.body = this.session.count;
+});
+*/
 
 router.get('/', function * (){
     this.body = '<form action="/get" method="get">'+
@@ -32,7 +36,10 @@ router.post('/', function * (){
     console.log(this.request.body.id);
 });
 
+//利用 middleware 來使用 get , post 等等方法
 app.use(router.middleware());
+
+
 
 app.listen(3000,function(){
     console.log("listening port 3000");
